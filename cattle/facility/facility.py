bdb82c26cd077d773ac1d7e164753ab1192220c5
David Grant <seizethedave@gmail.com>
2024-01-19 13:13:22 -0800

    More facility fleshing out.

--- cattle/facility/facility.py
+++ cattle/facility/facility.py
@@ -1,2 +1,15 @@
+"""
+Writing a new facility.
+Each facility needs the following operations:
+
+should_run
+run
+dry_run
+"""
+
 class Facility:
     pass
+
+class IdempotentFacility(Facility):
+    def should_run(self):
+        return True
--- cattle/facility/file.py
+++ cattle/facility/file.py
@@ -1,7 +1,73 @@
+import filecmp
+import os
+import shutil
+
 from cattle.facility.facility import Facility
 
-
-class FileFacility(Facility):
-    def __init__(self):
+class Chmod(Facility):
+    def __init__(self, path, mode):
         super(Facility).__init__()
+        self.path = path
+        self.mode = mode
 
+    def should_run(self):
+        return True # Chmod is always safe to reapply.
+
+    def run(self):
+        os.chmod(self.path, self.mode)
+
+    def dry_run(self):
+        return [f"chmod({self.path}, {oct(self.mode)})"]
+
+class Chown(Facility):
+    def __init__(self, path, owner_name=None, group_name=None):
+        super(Facility).__init__()
+        self.path = path
+        self.owner_name = owner_name
+        self.group_name = group_name
+
+    def should_run(self):
+        return True # Chown is always safe to reapply.
+
+    def run(self):
+        shutil.chown(self.path, self.owner_name, self.group_name)
+
+    def dry_run(self):
+        return [f"chown({self.path} user={self.owner_name}, group={self.group_name})"]
+
+class MakeDir(Facility):
+    def __init__(self, path):
+        super(Facility).__init__()
+        self.path = path
+
+    def should_run(self):
+        return True # Naively idempotent.
+
+    def run(self):
+        os.makedirs(self.path, exist_ok=True)
+
+    def dry_run(self):
+        return [f"make directory {self.path} (mode={self.mode}, owner={self.owner}, group={self.group})"]
+
+class InstallFile(Facility):
+    def __init__(self, sourcefile, dest):
+        """
+        A file installation facility.
+        >>> InstallFile("foo.txt", "/var/run/foo.txt")
+        """
+        super(Facility).__init__()
+        self.sourcepath = sourcefile
+        self.destpath = dest
+
+    def should_run(self):
+        filecmp.clear_cache()
+        try:
+            return not filecmp.cmp(self.sourcepath, self.destpath, shallow=False)
+        except FileNotFoundError:
+            return True
+
+    def run(self):
+        shutil.copyfile(self.sourcepath, self.destpath)
+
+    def dry_run(self) -> str:
+        return [f"install file {self.destpath}"]
